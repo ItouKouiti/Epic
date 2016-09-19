82a3d9c981dd9bd1de9930c6eb2fc497befb153b
ItouKouiti <itoukouiti@outlook.jp>
2016-09-19 21:31:30 +0900

    release1.2
    20160919

--- js/plugins.js
+++ js/plugins.js
@@ -6,6 +6,5 @@ var $plugins =
 {"name":"SpecialSkill","status":true,"description":"TPが一定以上の時だけ、出るバトルコマンドが設定することが出来る。","parameters":{"TP値":"100"}},
 {"name":"SpecialAttack","status":true,"description":"アニメーションを連続で表示して必殺技っぽい感じにする。","parameters":{}},
 {"name":"SimpleMsgSideView","status":true,"description":"サイドビューバトルで技/アイテムの名前のみ表示します。","parameters":{"displayAttack":"0","position":"1"}},
-{"name":"TouchUI","status":true,"description":"UIをタッチ操作に適したものへ変更します。","parameters":{"Menu Command Text":"メニュー","Previous Command Text":"前","Next Command Text":"次"}},
 {"name":"scrollText","status":true,"description":"文章のスクロール表示が出来ないの対応プラグイン","parameters":{}}
 ];
